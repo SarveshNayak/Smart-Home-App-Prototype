3e9e91693d9c7e92cd8a13a2b52b01d5e27b8e65
SarveshNayak <sarveshnayak1998@gmail.com>
2021-02-23 13:19:11 +0530

    added few buttons

--- lib/screenTv.dart
+++ lib/screenTv.dart
@@ -11,8 +11,100 @@ class ScreenTva extends State<ScreenTv> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 60.00,
+          backgroundColor: Colors.grey[700],
+          title: Text(
+            'Smart TV',
+            style: TextStyle(
+                fontSize: 25.00,
+                color: Colors.white,
+                fontFamily: 'Montserrat',
+                fontWeight: FontWeight.w300),
+          ),
+        ),
         body: Container(
-      child: Text("hello"),
-    ));
+            width: 500,
+            color: Colors.grey[900],
+            child: Column(children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.05,
+              ),
+              Row(children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.07,
+                ),
+                Container(
+                    width: MediaQuery.of(context).size.width * 0.2,
+                    height: MediaQuery.of(context).size.height * 0.06,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(20))),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.grey[700],
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.00),
+                        ),
+                      ),
+                      child: Icon(
+                        Icons.power_settings_new,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                      onPressed: () {},
+                    )),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.45,
+                ),
+                Container(
+                    width: MediaQuery.of(context).size.width * 0.2,
+                    height: MediaQuery.of(context).size.height * 0.06,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(20))),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.grey[700],
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.00),
+                        ),
+                      ),
+                      child: Icon(
+                        Icons.volume_off_outlined,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                      onPressed: () {},
+                    )),
+              ]),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.03,
+              ),
+              Row(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.42,
+                  ),
+                  Container(
+                      width: MediaQuery.of(context).size.width * 0.15,
+                      height: MediaQuery.of(context).size.height * 0.08,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(40))),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.grey[700],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(40.00),
+                          ),
+                        ),
+                        child: Icon(
+                          Icons.mic,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                        onPressed: () {},
+                      )),
+                ],
+              ),
+            ])));
   }
 }
