a36ddf413d6e54050b9ce4f07cdfcdff1ef9f87f
SarveshNayak <sarveshnayak1998@gmail.com>
2021-03-15 12:05:25 +0530

    Added the appbar and few sliders

--- lib/screenSpotlight.dart
+++ lib/screenSpotlight.dart
@@ -11,8 +11,27 @@ class ScreenSpotlighta extends State<ScreenSpotlight> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 60.00,
+          backgroundColor: Colors.grey[700],
+          title: Text(
+            'Smart Spotlight',
+            style: TextStyle(
+                fontSize: 25.00,
+                color: Colors.white,
+                fontFamily: 'Montserrat',
+                fontWeight: FontWeight.w300),
+          ),
+        ),
         body: Container(
-      child: Text("hello"),
-    ));
+          width: 500,
+            color: Colors.grey[900],
+          child:Column(
+            children:[
+              Text('hi'),
+            ]
+          )
+        )
+      );
   }
 }
